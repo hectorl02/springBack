5f1d106237e23bd1d835f95351c654a7ed078739
Hector Lopez <hectorlopezp02@gmail.com>
2022-03-30 15:27:35 -0500

    se crea productoController

---
+++ src/main/java/com/myback/hectorl/controllers/ProductoController.java
@@ -0,0 +1,57 @@
+package com.myback.hectorl.controllers;
+
+import com.myback.hectorl.models.Producto;
+import com.myback.hectorl.services.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = {"*"})
+@RestController
+@RequestMapping("api/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Producto>> listarProductos () {
+        return new ResponseEntity<>(productoService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<Producto> crearProducto (@RequestBody Producto producto) {
+        return new ResponseEntity<>(productoService.save(producto),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Producto> actualizarProducto(@PathVariable Long id, @RequestBody Producto producto){
+        Producto productoEncontrado = productoService.findById(id);
+
+        if (productoEncontrado == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            productoEncontrado.setNombre(producto.getNombre());
+            productoEncontrado.setPrecio(producto.getPrecio());
+            productoEncontrado.setImagen(producto.getImagen());
+            productoEncontrado.setCategoria(producto.getCategoria());
+            return  new ResponseEntity<>(productoService.save(productoEncontrado),HttpStatus.CREATED);
+
+        } catch (DataAccessException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Producto> eliminarProducto(@PathVariable Long id) {
+        productoService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    }
