8865e883d1f95ca404b0a2b67c95dba284059a1e
Hector Lopez <hectorlopezp02@gmail.com>
2022-03-30 12:39:58 -0500

    primer commit

---
+++ src/main/java/com/myback/hectorl/HectorlApplication.java
@@ -0,0 +1,13 @@
+package com.myback.hectorl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HectorlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HectorlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myback/hectorl/HectorlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myback.hectorl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HectorlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
